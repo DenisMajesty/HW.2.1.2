e09a4f93235000a28e5efbfa912378d6e5387f3d
Denis <majestycrew@gmail.com>
2023-03-04 14:47:04 +0300

    App full

--- HW.2.1.2/ViewController.swift
+++ HW.2.1.2/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CollorView {
+    case red, yellow, green
+}
 
+class ViewController: UIViewController {
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var collorView = CollorView.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redView.alpha = lightIsOff
+        yellowView.alpha = lightIsOff
+        greenView.alpha = lightIsOff
+        
+        redView.layer.cornerRadius = redView.frame.size.height / 2
+        yellowView.layer.cornerRadius = yellowView.frame.size.height / 2
+        greenView.layer.cornerRadius = greenView.frame.size.height / 2
+        
+        startButton.layer.cornerRadius = 20
     }
-
-
+    
+    @IBAction func pressesButton(_ sender: UIButton) {
+        startButton.setTitle("Next", for: .normal)
+        
+        switch collorView {
+        case .red:
+            greenView.alpha = lightIsOff
+            redView.alpha = lightIsOn
+            collorView = .yellow
+        case .yellow:
+            redView.alpha = lightIsOff
+            yellowView.alpha = lightIsOn
+            collorView = .green
+        case .green:
+            yellowView.alpha = lightIsOff
+            greenView.alpha = lightIsOn
+            collorView = .red
+        }
+    }
+    
 }
 
